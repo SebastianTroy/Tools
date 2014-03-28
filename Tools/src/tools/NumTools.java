59cc7f29b17fcbda4d3abb092deca8946b4ec389
Sebastian Troy <sebastiantroy@btinternet.com>
2014-03-28 10:23:01 +0000

    Improved methods

--- Tools/src/tools/NumTools.java
+++ Tools/src/tools/NumTools.java
@@ -4,7 +4,24 @@ public class NumTools
 	{
 		public static double distance(double x1, double y1, double x2, double y2)
 			{
-				return (Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2)));
+				return (Math.sqrt(((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2))));
+			}
+
+		public static double distanceSquare(double x1, double y1, double x2, double y2)
+			{
+
+				return ((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2));
+			}
+
+		public static double distance(double x1, double y1, double z1, double x2, double y2, double z2)
+			{
+				return (Math.sqrt(((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)) + ((z1 - z2) * (z1 - z2))));
+			}
+
+		public static double distanceSquare(double x1, double y1, double z1, double x2, double y2, double z2)
+			{
+
+				return ((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)) + ((z1 - z2) * (z1 - z2));
 			}
 
 		public static double[] getVector(double angle)
